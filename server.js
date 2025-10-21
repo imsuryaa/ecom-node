a1ecefc420d16f657b7f093d02a4fb9afa132959
imsuryaa <suryatejaj97@gmail.com>
2025-10-21 18:34:35 +0530

    cors issue by pass

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ import authRoutes from "./routes/auth.js";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  next();
+});
+
 app.use(express.json());
 
 // Mount auth routes
